75f00968e898ba7583bd6aed16531ea3e49b6a86
Boris Parak <parak@cesnet.cz>
2014-12-27 14:00:23 +0100

    [Azure] Added client init

--- lib/backends/azure_backend.rb
+++ lib/backends/azure_backend.rb
@@ -9,6 +9,17 @@ module Backends
       @logger = logger || Rails.logger
       @dalli_cache = dalli_cache
 
+      # configure and initialize client instances
+      ::Azure.configure do |config|
+        # configure virtual machine management access
+        config.management_certificate = @options.management_certificate
+        config.subscription_id        = @options.subscription_id
+        config.management_endpoint    = @options.management_endpoint || "https://management.core.windows.net"
+      end
+      init_client_instances
+
+      @options.backend_scheme ||= "http://occi.#{@server_properties.hostname || 'localhost'}"
+
       path = @options.fixtures_dir || ''
       read_fixtures(path)
     end
@@ -18,11 +29,18 @@ module Backends
       # TODO: impl reading fixtures
     end
 
+    def init_client_instances
+      @base_management_service ||= ::Azure::BaseManagementService.new
+      @virtual_machine_service ||= ::Azure::VirtualMachineManagementService.new
+      @virtual_machine_image_service ||= ::Azure::VirtualMachineImageManagementService.new
+    end
+
     # load helpers for JSON -> Collection conversion
     include Backends::Helpers::JsonCollectionHelper
 
     # hide internal stuff
     private :read_fixtures
+    private :init_client_instances
 
     # load API implementation
     include Backends::Azure::Compute
